4857e8c907d493d211c64fb14160cb050b3d0879
Yuriy <yfrolou@mail.ru>
2022-10-02 17:50:46 +0300

    release 1.0.7

--- script.js
+++ script.js
@@ -12,24 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
     let minutes = date.getMinutes() * 6;
     let hours = date.getHours() >= 12 ? (date.getHours() - 12) * 30 + (Math.round(date.getMinutes() / 12)) * 6 : date.getHours() * 30 + (Math.round(date.getMinutes() / 12)) * 6;
 
-
+    let count = 0;
     setInterval(() => {
+        const dateInner = new Date();
+        title.innerHTML = `${formatDate(dateInner)}`;
+        description.innerHTML = `${dateInner.toLocaleString()}`;
 
-        title.innerHTML = `${formatDate(new Date())}`;
-        description.innerHTML = `${new Date().toLocaleString()}`;
-
-        if (new Date().getHours() === 0) {
+        if (dateInner.getHours() === 0) {
             hours = 0;
         }
         if (seconds === 360) {
             seconds = 0;
             minutes += 6;
+            console.log(minutes)
         }
-        if (minutes % 72 === 0) {
-            hours += 6;
+        if (minutes === 72 || minutes === 144 || minutes === 216 || minutes === 288) {
+            count++;
+            if (count === 1) {
+                hours += 6;
+            }
         }
         if (minutes === 360) {
+            count++;
+            if (count === 1) {
+                hours += 6;
+            }
             minutes = 0;
+        }
+        if (count >= 60) {
+            count = 0;
         }
         seconds += 6;
         second.style.transform = `rotate(${seconds}deg)`;
